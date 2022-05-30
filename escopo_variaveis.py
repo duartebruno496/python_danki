6fce88b3a09d7813509f8ba487d77568629dbd72
Bruno Duarte <57137017+duartebruno496@users.noreply.github.com>
2022-05-30 23:43:11 +0000

    aulas danki code

---
+++ escopo_variaveis.py
@@ -0,0 +1,25 @@
+#Escopo de variaveis, sendo globais e locais 
+
+x = 5
+
+def funcao():
+    x = 3
+    print("Valor da variavel local: ",x)
+
+    funcao()
+    print("Valor da variavel global: ",x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
