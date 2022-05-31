8c1878bda69a637877f75cb7e6ec3157cd1d90a5
Bruno Duarte <57137017+duartebruno496@users.noreply.github.com>
2022-05-31 00:22:18 +0000

    Aula booleana

---
+++ boolean.py
@@ -0,0 +1,32 @@
+#################################
+# Operadores Lógicos e o tipo Boolean
+# Em ciência da computação, boolean, ou lógico, é um tipo 
+# de dado primitivo que possui dois valores, que podem ser considerador como (0 ou 1), falso ou verdadeiro
+# Chamado boolean e homenagem a George Boole
+# 
+# Operadores Lógicos
+#
+# and (e)
+# or (ou)
+# not (negação)
+# 
+# Tabela Verdade
+#
+# Utilizando "E"
+# Verdadeiro e Verdadeiro - True and True = True
+# Verdadeiro e Falso - True and False = False
+# Falso e Verdadeiro - False and True = False
+# Falso e Falso - False and False = False
+#
+# Utilizando "OU"
+# Verdadeiro ou Verdadeiro - True or True = True
+# Verdadeiro ou Falso - True or False = True
+# Falso ou Verdadeiro - False or True = True
+# Falso ou Falso - False or False = False
+#
+#
+#
+#################################
+
+
+
---
+++ operadores_aritmeticos.py
@@ -0,0 +1,51 @@
+# Operadores Aritméticos
+# nome_longo = 8
+# x = 5
+# y = 3
+#################################
+# Operador de atibuição =
+# Operador de soma ou adição +
+# Operador de subtração -
+# Operador de multiplicação *
+# Operador da divisão /
+# Operador de módulo (resto) %
+# Operador da exponenciação **
+# Operador da divisão inteira //
+#################################
+
+x = 3
+y = 5
+z = x + y
+print(z)
+
+x = 3
+y = 5
+z = y - x
+print(z)
+
+x = 3
+y = 5
+z = y * x
+print(z)
+
+x = 3
+y = 5
+z = y / x
+print(z)
+
+x = 3
+y = 5
+z = y ** x
+print(z)
+
+x = 3
+y = 5
+z = y // x
+print(z)
+
+x = 3
+y = 5
+z = y % x
+print(z)
+
+
