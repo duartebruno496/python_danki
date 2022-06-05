008618bdf499c8b72270ab9119134773630b333d
Bruno Duarte <duarte.bruno496@gmail.com>
2022-06-04 22:00:57 -0300

    Create condicional_if.py
    adicionado aula de condicional

---
+++ condicional_if.py
@@ -0,0 +1,20 @@
+"""
+Comandos de controle condicional 
+
+if, else, elif(else+if)
+
+"""
+
+#x = 5
+#y = 8
+
+x = 20
+y = 3
+
+if y > x:
+    #print("y é maior que x")
+    print("Código dentro do bloco condicional if")
+
+print("Código fora do bloco condicional")
+
+
