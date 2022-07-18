d2903af27b522040e70c0e4412beddbc6f1dead0
Bruno Duarte <57137017+duartebruno496@users.noreply.github.com>
2022-07-18 14:24:49 +0000

    lista slicing

---
+++ listas.py
@@ -0,0 +1,18 @@
+lista = ["chicago", "queens", "salvador", "pernambuco"]
+print(lista)
+
+lista2 = [2, 3, 7, 8, 10]
+print(lista2)
+
+lista3 = [2.0 , 3.5, 6.3]
+print(lista3)
+
+lista4 = [True, False]
+print(lista4)
+
+lista5 = [True, "chicago", 2.5, False, 4, 8]
+print(lista5)
+
+print(type(lista5))
+
+print(lista5[::])
